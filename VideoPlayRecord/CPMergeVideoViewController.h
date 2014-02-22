883a633400bd38dfaf14b3da6481fb7562d0e6e4
casspangell <casspangell@gmail.com>
2014-02-21 17:51:04 -0700

    User can now merge two video tracks and an audio track and save to album.

--- VideoPlayRecord/CPMergeVideoViewController.h
+++ VideoPlayRecord/CPMergeVideoViewController.h
@@ -7,7 +7,29 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import <CoreMedia/CoreMedia.h>
+#import <MobileCoreServices/UTCoreTypes.h>
+#import <AssetsLibrary/AssetsLibrary.h>
+#import <MediaPlayer/MediaPlayer.h>
 
 @interface CPMergeVideoViewController : UIViewController
+{
+    BOOL isSelectingAssetOne; // Used to identify which asset is currently selected
+}
+
+@property(nonatomic, strong) AVAsset *firstAsset;
+@property(nonatomic, strong) AVAsset *secondAsset;
+@property(nonatomic, strong) AVAsset *audioAsset;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityView;
+
+@property (weak, nonatomic) IBOutlet UIButton *loadAssetOne;
+@property (weak, nonatomic) IBOutlet UIButton *loadAssetTwo;
+@property (weak, nonatomic) IBOutlet UIButton *loadAudio;
+@property (weak, nonatomic) IBOutlet UIButton *mergeAndSave;
+
+-(BOOL)startMediaBrowserFromViewController:(UIViewController*)controller usingDelegate:(id)delegate;
+-(void)exportDidFinish:(AVAssetExportSession*)session;
+
 
 @end
--- VideoPlayRecord/CPMergeVideoViewController.m
+++ VideoPlayRecord/CPMergeVideoViewController.m
@@ -29,6 +29,183 @@
 	// Do any additional setup after loading the view.
 }
 
+#pragma mark - Loading Assets
+- (IBAction)loadAssetOnePressed:(id)sender {
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeSavedPhotosAlbum] == NO) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"No Saved Album Found" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
+        [alert show];
+    } else {
+        isSelectingAssetOne = TRUE;
+        [self startMediaBrowserFromViewController:self usingDelegate:self];
+    }
+}
+
+- (IBAction)loadAssetTwoPressed:(id)sender {
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeSavedPhotosAlbum] == NO) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"No Saved Album Found" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
+        [alert show];
+    } else {
+        isSelectingAssetOne = FALSE;
+        [self startMediaBrowserFromViewController:self usingDelegate:self];
+    }
+}
+
+-(BOOL)startMediaBrowserFromViewController:(UIViewController*)controller usingDelegate:(id)delegate {
+    
+    //Validation
+    if(([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeSavedPhotosAlbum] == NO) || (delegate == nil) || (controller == nil)){
+        return  NO;
+    }
+    
+    // Create video image picker
+    UIImagePickerController *mediaUI = [[UIImagePickerController alloc] init];
+    mediaUI.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+    mediaUI.mediaTypes = [[NSArray alloc] initWithObjects:(NSString*)kUTTypeMovie, nil];
+    
+    // Show controls for movie editing
+    mediaUI.allowsEditing = YES;
+    mediaUI.delegate = delegate;
+    
+    // Display image picker
+    [controller presentViewController:mediaUI animated:YES completion:nil];
+    
+    return YES;
+}
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info{
+    
+    // Get media type
+    NSString *mediaType = [info objectForKey:UIImagePickerControllerMediaType];
+    // Dismiss image picker
+    [self dismissViewControllerAnimated:NO completion:nil];
+    
+    // Handle video selection, grab type movie
+    if (CFStringCompare ((__bridge_retained CFStringRef) mediaType, kUTTypeMovie, 0) == kCFCompareEqualTo) {
+        
+        //  initialize each asset variable using the media URL returned by the image picker
+        if(isSelectingAssetOne){
+            NSLog(@"Video One Loaded");
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Asset Loaded" message:@"Video One Loaded"
+                                                           delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [alert show];
+            
+            _firstAsset = [AVAsset assetWithURL:[info objectForKey:UIImagePickerControllerMediaURL]];
+        } else {
+            NSLog(@"Video Two Loaded");
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Asset Loaded" message:@"Video Two Loaded"
+                                                           delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [alert show];
+            
+            _secondAsset = [AVAsset assetWithURL:[info objectForKey:UIImagePickerControllerMediaURL]];
+            
+        }
+    }
+}
+
+#pragma mark - Loading Audio
+// select audio files from your music library, you will use the MPMediaPickerController. It works exactly the same as UIImagePickerController, but instead of images and video, it accesses audio files in the media library.
+- (IBAction)loadAudioPressed:(id)sender {
+    MPMediaPickerController *mediaPicker = [[MPMediaPickerController alloc] initWithMediaTypes:MPMediaTypeAny];
+    mediaPicker.delegate = self;
+    mediaPicker.prompt = @"Select Audio";
+    [self presentViewController:mediaPicker animated:YES completion:nil];
+}
+
+-(void) mediaPicker:(MPMediaPickerController *)mediaPicker didPickMediaItems:(MPMediaItemCollection *)mediaItemCollection{
+    NSArray *selectedSong = [mediaItemCollection items];
+    if([selectedSong count] > 0){
+        MPMediaItem *songItem = [selectedSong objectAtIndex:0];
+        NSURL *songURL = [songItem valueForProperty:MPMediaItemPropertyAssetURL];
+        _audioAsset = [AVAsset assetWithURL:songURL];
+        
+        NSLog(@"Audio Loaded");
+        
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Asset Loaded" message:@"Audio Loaded"
+                                                       delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alert show];
+    }
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+-(void)mediaPickerDidCancel:(MPMediaPickerController *)mediaPicker {
+    [self dismissViewControllerAnimated: YES completion:nil];
+}
+
+#pragma mark - Merge Assests
+- (IBAction)mergeAndSavePressed:(id)sender {
+    if((_firstAsset != nil) && (_secondAsset != nil)){
+        [_activityView startAnimating];
+        
+        // Create AVMutableComposition object. This object will hold your AVMutableCompositionTrack instances.
+        AVMutableComposition *mixComposition = [[AVMutableComposition alloc] init];
+        
+        // Video Track
+        AVMutableCompositionTrack *firstTrack = [mixComposition addMutableTrackWithMediaType:AVMediaTypeVideo preferredTrackID:kCMPersistentTrackID_Invalid];
+
+        [firstTrack insertTimeRange:CMTimeRangeMake(kCMTimeZero, _firstAsset.duration)
+                            ofTrack:[[_firstAsset tracksWithMediaType:AVMediaTypeVideo] objectAtIndex:0] atTime:kCMTimeZero error:nil];
+        [firstTrack insertTimeRange:CMTimeRangeMake(kCMTimeZero, _secondAsset.duration)
+                            ofTrack:[[_secondAsset tracksWithMediaType:AVMediaTypeVideo] objectAtIndex:0] atTime:kCMTimeZero error:nil];
+        
+        // Audio Track
+        if (_audioAsset != nil){
+            AVMutableCompositionTrack *audioTrack = [mixComposition addMutableTrackWithMediaType:AVMediaTypeAudio preferredTrackID:kCMPersistentTrackID_Invalid];
+            [audioTrack insertTimeRange:CMTimeRangeMake(kCMTimeZero, CMTimeAdd(_firstAsset.duration, _secondAsset.duration))
+                                ofTrack:[[_audioAsset tracksWithMediaType:AVMediaTypeAudio] objectAtIndex:0] atTime:kCMTimeZero error:nil];
+        }
+        
+        // Get path
+        // Before you can save the final video, you need a path for the saved file. So create a random file name that points to a file in the documents folder.
+             NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+             NSString *documentsDirectory = [paths objectAtIndex:0];
+             NSString *myPathDocs =  [documentsDirectory stringByAppendingPathComponent:
+                                      [NSString stringWithFormat:@"mergeVideo-%d.mov",arc4random() % 1000]];
+             NSURL *url = [NSURL fileURLWithPath:myPathDocs];
+             
+             // Create exporter
+             // Render and export the merged video
+             AVAssetExportSession *exporter = [[AVAssetExportSession alloc] initWithAsset:mixComposition
+                                                                               presetName:AVAssetExportPresetHighestQuality];
+             exporter.outputURL=url;
+             exporter.outputFileType = AVFileTypeQuickTimeMovie;
+             exporter.shouldOptimizeForNetworkUse = YES;
+             [exporter exportAsynchronouslyWithCompletionHandler:^{
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [self exportDidFinish:exporter];
+                });
+            }];
+    }
+}
+      
+// Export is performed asynchronously, this method returns immediately
+-(void)exportDidFinish:(AVAssetExportSession*)session {
+ if (session.status == AVAssetExportSessionStatusCompleted) {
+     NSURL *outputURL = session.outputURL;
+     ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+     if ([library videoAtPathIsCompatibleWithSavedPhotosAlbum:outputURL]) {
+         [library writeVideoAtPathToSavedPhotosAlbum:outputURL completionBlock:^(NSURL *assetURL, NSError *error){
+             dispatch_async(dispatch_get_main_queue(), ^{
+                 if (error) {
+                     UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Video Saving Failed"
+                                                                    delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+                     [alert show];
+                 } else {
+                     UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Video Saved" message:@"Saved To Photo Album"
+                                                                    delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+                     [alert show];
+                 }
+             });
+         }];
+     } 
+ }
+    _audioAsset = nil;
+    _firstAsset = nil;
+    _secondAsset = nil;
+    [_activityView stopAnimating];
+}
+
+
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
