385744828ad8d07068ea82f9367f8ebfc4ddf046
casspangell <casspangell@gmail.com>
2014-03-04 11:22:25 -0700

    Added a new class

---
+++ CPRecordAudioViewController.h
@@ -0,0 +1,15 @@
+//
+//  CPRecordAudioViewController.h
+//  VideoPlayRecord
+//
+//  Created by mondousage on 3/3/14.
+//  Copyright (c) 2014 MondoLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPRecordAudioViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIButton *recordAudioButton;
+
+@end
---
+++ CPRecordAudioViewController.m
@@ -0,0 +1,42 @@
+//
+//  CPRecordAudioViewController.m
+//  VideoPlayRecord
+//
+//  Created by mondousage on 3/3/14.
+//  Copyright (c) 2014 MondoLabs. All rights reserved.
+//
+
+#import "CPRecordAudioViewController.h"
+
+@interface CPRecordAudioViewController ()
+
+@end
+
+@implementation CPRecordAudioViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (IBAction)recordAudioPressed:(id)sender {
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
